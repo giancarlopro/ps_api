0c1e743cc671712d38a648ac07bdb804437082e7
Giancarlo Rocha <giancarloiff@gmail.com>
2020-05-05 14:26:13 -0300

    add companies CRUD

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,20 @@
 class Company < ApplicationRecord
-    has_one_attached :logo
+  include PgSearch::Model
+  pg_search_scope :search, 
+                  against: [:company_name, :fantasy_name, :email],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
+
+  has_one_attached :logo
+
+  STATUS_OPTIONS = [['Ativa', 'ativa'], ['Inativa', 'inativa']]
+
+  has_and_belongs_to_many :categories
+
+  validates :company_name, presence: true
+  validates :fantasy_name, presence: true
+  validates :category_ids, presence: true
+  validates :email, presence: true
+  validates_email_format_of :email
 end
