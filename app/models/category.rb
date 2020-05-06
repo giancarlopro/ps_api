f9d223e763ef15b49879ad67f8fed0f0e3889350
Giancarlo Rocha <giancarloiff@gmail.com>
2020-05-06 10:26:37 -0300

    add pg_search to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,12 @@
 class Category < ApplicationRecord
-    has_one_attached :image
+  include PgSearch::Model
+  pg_search_scope :search, 
+                  against: :name,
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 
-    has_and_belongs_to_many :companies
+  has_one_attached :image
+
+  has_and_belongs_to_many :companies
 end
