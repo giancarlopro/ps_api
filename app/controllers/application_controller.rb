9d51822bee41794156c68e5bffc254bd04f9efe2
Giancarlo Rocha <giancarloiff@gmail.com>
2020-05-09 22:19:40 -0300

    add user name to top bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :request_change_password?
+  before_action :set_user
   
-  def request_change_password?
-    if current_company_user and current_company_user.request_change_password
-      redirect_to edit_company_users_path
+  protected
+    def request_change_password?
+      if current_company_user and current_company_user.request_change_password
+        redirect_to edit_company_users_path
+      end
     end
-  end
+
+    def set_user
+      @current_user = current_company_user || current_admin_user
+    end
 end
